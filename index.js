c0aac7863cf073e58fcfb6f945c9845d243e7c2e
Michal Molenda <michalus88@gmail.com>
2021-12-31 23:35:48 +0100

    added view engine with config

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
 
 const app = express();
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
 
 app.get("/",(req,res)=>{
     res.send("home")
