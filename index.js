673b74b3ecd6bf852f0935fa826073f648f8050c
Michal Molenda <michalus88@gmail.com>
2022-01-01 00:24:24 +0100

    added and  used childrenRouter

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
 
+const childrenRouter = require('./routes/childrenRouter');
+
 const app = express();
 
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
-app.get("/",(req,res)=>{
-    res.send("home")
-})
+app.get('/', (req, res) => {
+  res.send('home');
+});
+app.use('/children', childrenRouter());
 
-app.listen(3000)
+app.listen(3000);
