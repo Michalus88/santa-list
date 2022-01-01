779c348ead058c23a10fc859a2b7251e88c78017
Michal Molenda <michalus88@gmail.com>
2022-01-02 00:49:23 +0100

    changed value mok data

--- mokData.js
+++ mokData.js
@@ -1,5 +1,5 @@
 const children = [{ firstName: 'Michał', gifts: [] }, { firstName: 'Franek', gifts: [] }, { firstName: 'Karolina', gifts: [] }];
-const gifts = [{ name: 'kolejka', amount: 10 }, { name: 'klocki', amount: 10 }, { name: 'bączek', amount: 10 }];
+const gifts = [{ name: 'kolejka', amount: 2 }, { name: 'klocki', amount: 10 }, { name: 'bączek', amount: 10 }];
 
 const add = () => {
   children.forEach((child) => (
