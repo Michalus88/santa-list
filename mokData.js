5fc363943d2deb54880881f9b851b92c9132432b
Michal Molenda <michalus88@gmail.com>
2022-01-01 13:31:51 +0100

    changed data structure and create add gift function

--- mokData.js
+++ mokData.js
@@ -1,2 +1,13 @@
-const children = ["Michał", "Franek", "Karolina"];
-const gifts = [{name:"kolejka", amount:10},{name:"klocki", amount: 10},{name:"bączek",amount: 10}]
+const children = [{ firstName: 'Michał', gifts: [] }, { firstName: 'Franek', gifts: [] }, { firstName: 'Karolina', gifts: [] }];
+const gifts = [{ name: 'kolejka', amount: 10 }, { name: 'klocki', amount: 10 }, { name: 'bączek', amount: 10 }];
+
+const add = () => {
+  children.forEach((child) => (
+    child.firstName === 'Michał' ? child.gifts.push('kolejka') : null));
+};
+
+module.exports = {
+  children,
+  gifts,
+  add,
+};
