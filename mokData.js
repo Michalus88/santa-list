348bc3aba0dd7af9760b9937f1ff1b657625596b
Michal Molenda <michalus88@gmail.com>
2022-01-02 00:53:50 +0100

    added checking of stock amount

--- mokData.js
+++ mokData.js
@@ -1,9 +1,17 @@
 const children = [{ firstName: 'Michał', gifts: [] }, { firstName: 'Franek', gifts: [] }, { firstName: 'Karolina', gifts: [] }];
 const gifts = [{ name: 'kolejka', amount: 2 }, { name: 'klocki', amount: 10 }, { name: 'bączek', amount: 10 }];
 
-const add = () => {
-  children.forEach((child) => (
-    child.firstName === 'Michał' ? child.gifts.push('kolejka') : null));
+const add = (name, item) => {
+  let lack = true;
+
+  gifts.forEach((gift) => (gift.name === item
+    ? gift.amount === 0
+      ? lack = false : --gift.amount
+    : null
+  ));
+  if (lack) {
+    children.forEach((child) => (child.firstName === name ? child.gifts.push(item) : null));
+  }
 };
 
 module.exports = {
