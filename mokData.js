3b07e6f7e24520729726a6e5940712ecac83b3ea
Michal Molenda <michalus88@gmail.com>
2022-01-03 22:29:55 +0100

    refactor: split code replace gifts arra to GiftRecord class

--- mokData.js
+++ mokData.js
@@ -1,5 +1,4 @@
 const children = [{ firstName: 'Michał', gifts: [] }, { firstName: 'Franek', gifts: [] }, { firstName: 'Karolina', gifts: [] }];
-const items = [{ name: 'kolejka', amount: 2 }, { name: 'klocki', amount: 10 }, { name: 'bączek', amount: 10 }];
 
 class Child {
   constructor(name, gifts = []) {
@@ -9,30 +8,34 @@ class Child {
 
   static async findOne(name) {
     const foundChild = children.filter((child) => child.firstName === name)[0];
-
+    console.log(foundChild);
     return new Child(foundChild.firstName, foundChild.gifts);
   }
 
   static async findAll() {
-    return children.map(({ firstName, gifts }) => new Child({ firstName, gifts }));
+    return children.map(({ firstName, gifts }) => new Child(firstName, gifts));
   }
 
-  addGift = (item) => {
-    let lack = true;
-
-    items.forEach((gift) => (gift.name === item
-      ? gift.amount === 0 || gift.name === undefined
-        ? lack = false : --gift.amount
-      : lack = false
-    ));
-    if (lack) {
-      this.gifts.push(item);
+  addGift = (isAvailable, itemName) => {
+    if (isAvailable) {
+      this.gifts.push(itemName);
+    } else {
+      console.error('produkt niedostępny');
     }
+    // let lack = true;
+    //
+    // items.forEach((gift) => (gift.name === item
+    //   ? gift.amount === 0 || gift.name === undefined
+    //     ? lack = false : --gift.amount
+    //   : lack = false
+    // ));
+    // if (lack) {
+    //   this.gifts.push(item);
+    // }
   };
 }
 
 module.exports = {
   Child,
   children,
-  gifts: items,
 };
