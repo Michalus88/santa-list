b039ed1a2097149910e2ce79531715108d7a5242
Michal Molenda <michalus88@gmail.com>
2022-01-08 20:12:33 +0100

    clear:removed mok methood

--- records/ChildRecord.js
+++ records/ChildRecord.js
@@ -1,4 +1,4 @@
-const children = [{ firstName: 'Michał', gifts: [] }, { firstName: 'Franek', gifts: [] }, { firstName: 'Karolina', gifts: [] }];
+
 
 class Child {
   constructor(name, gifts = []) {
@@ -10,22 +10,22 @@ class Child {
   }
 
   static async findOne(name) {
-    const foundChild = children.filter((child) => child.firstName === name)[0];
+    const foundChild =
 
     return new Child(foundChild.firstName, foundChild.gifts);
   }
 
   static async findAll() {
-    return children.map(({ firstName, gifts }) => new Child(firstName, gifts));
+
   }
 
   static async addNew(name) {
-    children.push(new Child(name, []));
+
   }
 
    addGift = async (isAvailable, itemName) => {
      if (isAvailable) {
-       this.gifts.push(itemName);
+
      } else {
        throw new Error('Produkt niedostępny');
      }
