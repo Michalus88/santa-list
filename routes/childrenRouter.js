5631936af76d2722765002d5df9a8d16cd691d9b
Michal Molenda <michalus88@gmail.com>
2022-01-09 23:06:15 +0100

    cleaned up and added methood to get all children with gifts from DB

--- routes/childrenRouter.js
+++ routes/childrenRouter.js
@@ -1,26 +1,27 @@
 const { Router } = require('express');
 
-const { Child } = require('../records/ChildRecord');
+const { ChildRecord } = require('../records/ChildRecord');
 const { GiftRecord } = require('../records/GiftRecord');
 
 module.exports = () => {
   const childrenRouter = Router();
 
   childrenRouter.get('/', async (req, res) => {
-    const children = await Child.findAll();
-    // console.log(children);
+    const children = await ChildRecord.findAll();
+
+    console.log(children);
     const gifts = await GiftRecord.findAll();
     res.render('children/list', { children, gifts });
   });
 
   childrenRouter.post('/', async (req, res) => {
     const { newChild } = req.body;
-    await Child.addNew(newChild);
+    await ChildRecord.addNew(newChild);
     res.redirect('/children');
   });
 
   childrenRouter.post('/:id/gifts', async (req, res) => {
-    const child = await Child.findOne(req.params.name);
+    const child = await ChildRecord.findOne(req.params.name);
     const gift = await GiftRecord.findOne(req.body.item);
     if (gift) {
       const [isAvailable, itemName] = await gift.quantityDecrement();
