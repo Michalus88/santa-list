be46994abf619e26f6a11509e4cfcbc61159421c
Michal Molenda <michalus88@gmail.com>
2022-01-09 23:03:52 +0100

    clean:removed testing methood from GiftRecord

--- records/GiftRecord.js
+++ records/GiftRecord.js
@@ -1,30 +1,26 @@
-const items = [{ name: 'kolejka', amount: 2 }, { name: 'klocki', amount: 10 }, { name: 'bączek', amount: 10 }];
-
 class GiftRecord {
-  constructor(name, quantity) {
+  constructor(name, count) {
     if (name === undefined || name.length < 3) {
       throw new Error('Nazwa musi zawierać co najmniej 3 znaki');
     }
-    if (quantity === undefined || typeof quantity !== 'number') {
+    if (count === undefined || typeof count !== 'number') {
       throw new Error('Ilość jest wymagana i wartość musi być liczbą');
     }
 
     this.name = name;
-    this.amount = quantity;
+    this.count = count;
   }
 
   static async findOne(itemName) {
-    const gift = items.filter((item) => item.name === itemName)[0];
 
-    return gift === undefined ? null : new GiftRecord(gift.name, gift.amount);
   }
 
   static async findAll() {
-    return items.map(({ name, amount }) => new GiftRecord(name, amount));
+
   }
 
   static async add(name, quantity) {
-    items.push(new GiftRecord(name, quantity));
+
   }
 
   async quantityDecrement() {
