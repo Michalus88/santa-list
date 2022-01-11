51bef16abe3e38203146e64b35c482369e33df80
Michal Molenda <michalus88@gmail.com>
2022-01-11 21:03:03 +0100

    fead:added id to GiftRecord

--- records/GiftRecord.js
+++ records/GiftRecord.js
@@ -1,14 +1,14 @@
 const { pool } = require('../config/mariaDb');
 
 class GiftRecord {
-  constructor(name, count) {
+  constructor(id, name, count) {
     if (name === undefined || name.length < 3) {
       throw new Error('Nazwa musi zawierać co najmniej 3 znaki');
     }
     if (count === undefined || typeof count !== 'number') {
       throw new Error('Ilość jest wymagana i wartość musi być liczbą');
     }
-
+    this.id = id;
     this.name = name;
     this.count = count;
   }
