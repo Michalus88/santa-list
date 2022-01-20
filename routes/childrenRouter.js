15f7e8752626a34f8cf54621a089b5f373c288d6
Michal Molenda <michalus88@gmail.com>
2022-01-20 11:54:40 +0100

    implemented catch asynk function in childrenRouter

--- routes/childrenRouter.js
+++ routes/childrenRouter.js
@@ -2,35 +2,36 @@ const { Router } = require('express');
 
 const { ChildRecord } = require('../records/ChildRecord');
 const { GiftRecord } = require('../records/GiftRecord');
+const { catchAsync } = require('../utils/errors');
 
 module.exports = () => {
   const childrenRouter = Router();
 
-  childrenRouter.get('/', async (req, res) => {
+  childrenRouter.get('/', catchAsync(async (req, res) => {
     const children = await ChildRecord.findAll();
     const gifts = await GiftRecord.findAll();
 
     res.render('children/list', { children, gifts });
-  });
+  }));
 
-  childrenRouter.post('/', async (req, res) => {
+  childrenRouter.post('/', catchAsync(async (req, res) => {
     const newChild = new ChildRecord(req.body);
     await newChild.insert();
 
     res.redirect('/children');
-  });
+  }));
 
-  childrenRouter.post('/:id/gifts', async (req, res) => {
+  childrenRouter.post('/:id/gifts', catchAsync(async (req, res) => {
     const { giftId } = req.body;
     if (giftId === '') return res.redirect('/children');
 
-    const child = await ChildRecord.findOne(req.params.id);
-    const gift = await GiftRecord.findOne(giftId);
-    await gift.isGiftAvailable();
-    await child.addGift(giftId);
+    const child = await ChildRecord.findOne('232324244');
+    // const gift = await GiftRecord.findOne(giftId);
+    // await gift.isGiftAvailable();
+    // await child.addGift(giftId);
 
     res.redirect('/children');
-  });
+  }));
 
   return childrenRouter;
 };
